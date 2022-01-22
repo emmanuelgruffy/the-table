7a45dd2a168fc2bc4be692b6132fd6adfa42d18a
Emmanuel Gruffy <emmanuelgruffy@gmail.com>
2022-01-22 17:09:17 +0200

    background picture adjustment

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState } from "react";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Moment from "react-moment";
